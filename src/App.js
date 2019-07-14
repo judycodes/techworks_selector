fa8200b81a3d588b580a4e6a55c00d66c0728128
Judy <codelinked@gmail.com>
2019-07-14 19:21:29 -0400

    design implemented

--- src/App.js
+++ src/App.js
@@ -6,36 +6,46 @@ class Selector extends Component {
   constructor(){
     super();
     this.state = {
-      selectedShape : 'shape?', //initalized with a value and data type, which is string, for selectedShape
-
-
+      selectedShape : ' Shape?', //initalized with a value and data type, which in this case is a string value for selectedShape (key)
     };
   }
 
-//below allows for shape data to change...this is a method
+//below allows for shape data to change due to value passed as parameter...this is a method
   selectShape = (shapeName) => {
     this.setState({
       selectedShape: shapeName,
     })  
   }
-//in the Shape in jsx below, the selectShape attribute is calling the above selectShape method
-//this allows for this.state.selectedShape in Selected: div to change on click
+
+//in the Shape component in jsx below, the selectShape attribute is calling the above selectShape method to change local state data
+//this allows for this.state.selectedShape in 'This is called a shape?!' to change on click of various shape figures on browser page
 
 //state and props are objects with key/value pairs
 
   render(){
-    //shape = "shape/square/circle/triangle" creating shape property with a value. This becomes a passing props in shape component on Shape.js
+    //shape = "shape/square/circle/triangle/etc" is a shape property/key with a value. This becomes props in Shape.js/functional component as a parameter to be passed through function
     
     return (
       <div className = "container">
         <div className ="navbar">
-        <div>Selected: <span>{this.state.selectedShape}</span></div>
+          <h1 className = "hello">HELLO</h1>
+          <h2 className ="text">My Name Is <br />
+          <span className = "figureName" >{this.state.selectedShape}</span>!</h2>
         </div>
         
         <div className ="shape-list">
-          <Shape shape="square" selectShape = {this.selectShape}/> 
-          <Shape shape="circle" selectShape = {this.selectShape}/>
-          <Shape shape="triangle" selectShape = {this.selectShape}/>
+          <div className="row">
+          <Shape shape="square" className="shape" selectShape = {this.selectShape}/> 
+          <Shape shape="circle" className="shape" selectShape = {this.selectShape}/>
+          <Shape shape="triangle" className="shape" selectShape = {this.selectShape}/>
+          </div>
+
+          <div className="row">
+          <Shape shape="trapezium" className="shape" selectShape = {this.selectShape}/>
+          <Shape shape="star" className="shape" selectShape = {this.selectShape}/>
+          <Shape shape="oval" className="shape" selectShape = {this.selectShape}/>
+          </div>
+
         </div>
       </div>
     );
--- src/Shape.js
+++ src/Shape.js
@@ -1,21 +1,19 @@
-//functional component
+//Function  (stateless functional component)
+//no local state
 
 import React from 'react'
 
-const Shape = (props) => { //sends shape property's values
-//console.log(props.shape) should log in console: square, circle, triangle, which are values 
-//{props.shape} makes dynamic css values based on shape property values when used/assigned to className 
+const Shape = (props) => { //props are values located in app.js
 
-    const shape = props.shape; //values are square, circle, triangle - given in jsx of app.js
-    const selectShape = props.selectShape;
+    const shape = props.shape; //props.shape values are square, circle, triangle, and so on
+    const selectShape = props.selectShape; //selectShape method in app.js stored as a value in this variable
 
     return (
 
-        //props.shape is from shape property in app.js
-        //onClick fires when shape div is clicked on browser, calling an anonymous function 
-        //this anonymous fn takes in shape name, changing state in jsx of app.js and as displayed in between span of browser
+        //onClick fires when shape is clicked by user on browser, calls an anonymous function 
+        //this anonymous fn takes in shape name of figure clicked, changing state (value of shapeName) of selectedShape value in app.js, which is then displayed between span in .text h1 and in browser
         <div className = {shape} onClick = {() => selectShape(shape)} ></div>
-        //can be rewritten as <div className = {shape} onClick = {() => selectShape(shape)} />
+        //can also be rewritten as <div className = {shape} onClick = {() => selectShape(shape)} />
     );
 }
 
